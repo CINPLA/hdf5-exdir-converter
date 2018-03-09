8a1642a04e68c7bdb8a6ee41d56e937ac9e2f079
Milad <miladh@ibv.uio.no>
2018-03-09 10:20:29 +0100

    WIP: core

--- hdf5_exdir_converter/core.py
+++ hdf5_exdir_converter/core.py
@@ -5,33 +5,40 @@ import h5py
 def convert_attributes(src, target):
     for key, value in dict(src.attrs).items():
         target.attrs[key] = value
-    
-    
+
+
 def convert_dataset(src, target):
     convert_attributes(src=src, target=target)
-    dset = target.require_dataset(name=src.name, 
-                                  data=src.data,
-                                  shape=src.data.shape,
-                                  dtype=src.data.dtype)
-    
-    
-def convert_group(src, target):
+    dset = target.require_dataset(name=src.name,
+                                  data=src.value,
+                                  shape=src.value.shape,
+                                  dtype=src.value.dtype)
+
+
+def convert_group(src, target, module):
     convert_attributes(src=src, target=target)
 
     for name, item in src.items():
-        if isinstance(item, exdir.core.Group):
+        if isinstance(item, module.Group):
             grp = target.create_group(name=name)
-            convert_group(src=item, target=grp)
-            
-        elif isinstance(item, exdir.core.Dataset):
+            convert_group(src=item, target=grp, module=module)
+
+        elif isinstance(item, module.Dataset):
             convert_dataset(src=item, target=target)
 
 
-def convert(src_file, target):
-    # TODO: check filename extension
-    # TODO: check type (exdir/hdf5)
-    
+def convert(src_path, target_path):
+    if src_path.endswith(".exdir"):
+        src = exdir.File(src_path, 'r')
+        target = h5py.File(target_path, 'w')
+        module = exdir.core
+
+    elif src_path.endswith(".hdf5"):
+        src = h5py.File(src_path, 'r')
+        target = exdir.File(target_path, 'w')
+        module = h5py
+
     convert_attributes(src=src, target=target)
     for name, item in src.items():
         grp = target.create_group(name=name)
-        convert_group(src=item, target=grp)
+        convert_group(src=item, target=grp, module=module)
