5c5aed2bf3ca1b242d0fd4c7c6c01eb83f5a7120
Milad <miladh@ibv.uio.no>
2018-03-09 14:44:34 +0100

    ENH: cli updates

--- hdf5_exdir_converter/main.py
+++ hdf5_exdir_converter/main.py
@@ -3,13 +3,18 @@ import click
 
 @click.command()
 @click.help_option('-h', '--help')
-@click.argument("src",
-                type=click.STRING)
-@click.argument("target",
-                type=click.STRING)
+@click.option("--src",
+              "-s",
+              type=click.STRING,
+              help="source filename",
+              required=True)
+@click.option("--target",
+              "-t",
+              type=click.STRING,
+              help="target filename",
+              required=True)
 def cli(src, target):
     import hdf5_exdir_converter
-
     hdf5_exdir_converter.convert(src_path=src,
                                  target_path=target)
 
